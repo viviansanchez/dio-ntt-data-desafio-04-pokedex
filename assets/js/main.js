06d314a30b7002cc1a331ec95276a0754a155b87
Vivian Sanchez Desktop <visasal88@gmail.com>
2022-11-23 17:31:15 -0300

    projeto finalizado

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('load-more-button')
+const limit = 10
+let offset = 0
+
+function loadPokemonItems (offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons =[]) => { 
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img
+                        src="${pokemon.photo}"
+                        alt="${pokemon.name}"
+                    />
+                </div>
+            </li>
+        `).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItems(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItems(offset, limit)
+})
+
+// funcionalidade para limitar os pokemons a apenas pokemons da primeira geração (151 pokemons)
+
+// const maxRecords = 151
+
+// loadMoreButton.addEventListener('click', () => {
+//     offset += limit
+//     const qtdRecordsNextPage = offset + limit
+
+//     if (qtdRecordsNextPage >= maxRecords) {
+//         const newLimit = maxRecords - offset
+//         loadPokemonItems(offset, newLimit)
+
+//         loadMoreButton.parentElement.removeChild(loadMoreButton)
+//     } else {
+//         loadPokemonItems(offset, limit)
+//     }
+// })
+    
+
